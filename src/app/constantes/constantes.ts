efc9b42b255894700aed7a3d8d4b4acdff3dd6df
EdwinR <3.edwin.3@gmail.com>
2019-12-27 03:28:59 -0500

    Heroku deploy
    Configuraciones para despliegue en heroku

--- src/app/constantes/constantes.ts
+++ src/app/constantes/constantes.ts
@@ -1,2 +1,3 @@
-export const API_ENDPOINT = 'http://127.0.0.1:8000/api/v1/';
+// export const API_ENDPOINT = 'http://127.0.0.1:8000/api/v1/';
+export const API_ENDPOINT = 'https://compras-back.herokuapp.com/api/v1/';
 
